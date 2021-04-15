a939be2971bf043d3a637a99d639d491946f6b10
Nehemiah Boone <nehemiah.boone@gmail.com>
2021-04-15 14:56:46 -0600

    Add project files.

---
+++ EarlBot/Modules/GeneralModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarlBot.Modules
+{
+    public class GeneralModule : ModuleBase
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await Context.Channel.SendMessageAsync("Pong!");
+        }
+
+        [Command("info")]
+        public async Task Userinfo()
+        {
+            var builder = new EmbedBuilder()
+                .WithThumbnailUrl(Context.User.GetAvatarUrl() ?? Context.User.GetDefaultAvatarUrl())
+                .WithColor(new Color(33, 176, 252))
+                .AddField("Id", Context.User.Discriminator, true)
+                .AddField("Created at", Context.User.CreatedAt.ToString("MMMM dd, yyyy"))
+                .WithCurrentTimestamp();
+            var embed = builder.Build();
+            await Context.Channel.SendMessageAsync(null, false, embed);
+        }
+    }
+}
---
+++ EarlBot/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EarlBot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+    }
+}
